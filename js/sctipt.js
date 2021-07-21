08a4402ee5ed04f0cfb7edc164de39d98a168f5b
airat <you@example.com>
2021-07-21 22:26:01 +0300

    first commit
    Первый урок

---
+++ js/sctipt.js
@@ -0,0 +1,17 @@
+
+// alert ('hello');
+
+// const resoult=confirm ('are u here?');
+// console.log (resoult);
+
+// const answer=prompt ('Вам есть 18?', '18');
+// console.log (answer);
+
+// const answers = [];
+
+// answers[0] = prompt('Как ваше имя?', '');
+// answers[1] = prompt('Как ваше имя?', '');
+// answers[2] = prompt ('Сколько вам лет', '18');
+
+const user = 'Ivan';
+alert (`Привет, ${user}`);
